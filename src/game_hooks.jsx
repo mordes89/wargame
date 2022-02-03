58524a1dc76835d990471edcdfc0d7b089a0a2b7
mordes89 <mordes89@gmail.com>
2022-02-03 14:08:18 -0500

    createdeck, dealcards, playcards. Need to implement war logic

--- src/game_hooks.jsx
+++ src/game_hooks.jsx
@@ -1,58 +1,76 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 // import Deck from './deck';
 import { connect } from 'react-redux'
 
 
 const GameHooks = (props) => { 
    const [deck, setDeck] = useState([]);
+   // const [deck2, setDeck2] = useState([]);
    const [player1_hand, setPlayer1_hand] = useState([]);
    const [player2_hand, setPlayer2_hand] = useState([]);
    const [cards_to_be_won, setCards_to_be_won] = useState([]);
    const [play, setPlay] = useState(false);
    const [speed, setSpeed] = useState(1000);
-   const [game_on, setGame_on] = useState(false);    
+   const [game_on, setGame_on] = useState(false);  
+   
+   
+   useEffect(() => {
+      console.log('useEffect');
+      // setDeck2([deck])
+      // console.log('deck2',deck2);
+   },[deck])
 
+
+   const delete_card_from_deck = (index) => {
+      let tempDeck = deck;
+      let card = deck[index];
+      tempDeck.splice(index, 1);
+      setDeck([tempDeck])
+      return card;
+   }
+
+   const createDeck = () => {
+      // Create a new deck
+      let suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+      let values = [["Two", 2], ["Three", 3], ["Four", 4], ["Five", 5], ["Six", 6], ["Seven", 7], ["Eight", 8], ["Nine", 9], ["Ten", 10], ["Jack", 11], ["Queen", 12], ["King", 13], ["Ace", 14]]
+      for (let i = 0; i < values.length; i++) {
+         for (let j = 0; j < suits.length; j++) {
+            setDeck((d) => [...d, [`${values[i][0]} of ${suits[j]}`, values[i][1]]])
+         }
+      }  
+      console.log('deck created within dealcards()');
+      console.log('deck',deck);
+   }
    
 
    const dealCards = () => {   
       if (player1_hand.length === 0 && player2_hand.length === 0){
          
-         // Create a new deck
-         let suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
-         let values = [["Two", 2], ["Three", 3], ["Four", 4], ["Five", 5], ["Six", 6], ["Seven", 7], ["Eight", 8], ["Nine", 9], ["Ten", 10], ["Jack", 11], ["Queen", 12], ["King", 13], ["Ace", 14]]
-         for (let i = 0; i < values.length; i++) {
-            for (let j = 0; j < suits.length; j++) {
-               setDeck((d) => [...d, [`${values[i][0]} of ${suits[j]}`, values[i][1]]])
-            }
-         }  
-         console.log('deck created within dealcards()');
-         console.log('deck',deck);
-
-
          // Deal the cards from the deck to both players
-         let i = 0; 
-         //continue dealing as long as there are still cards in the deck  
-         
-         while (deck.length > 0){ 
+         //continue dealing as long as there are still cards in the deck
+         for (let i = 0; i<52; i++){ 
+            // let temp_deck = deck;
             //find a random card in the deck
             let random_card = Math.floor(Math.random()*deck.length);
-            if (i%2 === 0){ // deal to player 1 if i is even
-               // temp_player1_hand.push(temp_deck.splice(random_card, 1)[0])
-               setPlayer1_hand((hand1) => [...hand1, deck.splice(random_card, 1)[0]])
-            } else {       // deal to player 2 if i is uneven
-               // temp_player2_hand.push(temp_deck.splice(random_card, 1)[0])
-               setPlayer2_hand((hand2) => [...hand2, deck.splice(random_card, 1)[0]])
-
+            console.log('i',i);
+            console.log('random_card',random_card);
+            console.log('deck[random_card]',deck[random_card]);
+            if (i%2 === 0){               // deal to player 1 if i is even
+               let card1 = deck[random_card];
+               deck.splice(random_card, 1);               
+               setDeck((d)=> deck)
+               setPlayer1_hand((hand1) => [...hand1, card1])
+            } else if (i%2 === 1){       // deal to player 2 if i is uneven
+               let card2 = deck[random_card];
+               deck.splice(random_card, 1);
+               setDeck((d)=> deck)
+               setPlayer2_hand((hand2) => [...hand2, card2])
             }            
-            i++;
-         }
-         
-         console.log('dealCards1()');
-         console.log('player1_hand',player1_hand);
-         console.log('player2_hand',player2_hand);
-         console.log('deck',deck);
+         }         
       }   
-      console.log('dealCards2()');
+      console.log('player1_hand',player1_hand);
+      console.log('player2_hand',player2_hand);
+      console.log('deck',deck);
 
    }
 
@@ -72,6 +90,7 @@ const GameHooks = (props) => {
          reset();    
          return     
       }
+      setGame_on(()=>true)
       console.log('check_winner()');
    }
 
@@ -83,44 +102,64 @@ const GameHooks = (props) => {
       if (game_on){
          if (player1_hand[0][1] > player2_hand[0][1]){
             console.log('player1 wins');
-            setPlayer1_hand([...player1_hand, player2_hand.splice(0, 1)[0], player1_hand.splice(0, 1)[0] ])
 
-            while (cards_to_be_won.length > 0){
-               setPlayer1_hand([...player1_hand, cards_to_be_won.splice(0, 1)[0] ])
-            }
+            let card1 = player1_hand.splice(0, 1)[0];
+            let card2 = player2_hand.splice(0, 1)[0];
+             
+            setPlayer1_hand((h1)=> player1_hand)
+            setPlayer2_hand((h2)=> player2_hand)
+
+            setPlayer1_hand((hand1) => [...hand1, card1, card2])
+
+
+            // setPlayer1_hand([...player1_hand, player2_hand.splice(0, 1)[0], player1_hand.splice(0, 1)[0] ])
+
+            // while (cards_to_be_won.length > 0){
+            //    setPlayer1_hand([...player1_hand, cards_to_be_won.splice(0, 1)[0] ])
+            // }
          } else if (player1_hand[0][1] < player2_hand[0][1]){
             console.log('player2 wins');
-            setPlayer2_hand([...player2_hand, player2_hand.splice(0, 1)[0], player1_hand.splice(0, 1)[0] ])
             
-            while (cards_to_be_won.length > 0){
-               setPlayer2_hand([...player2_hand, cards_to_be_won.splice(0, 1)[0] ])
-            }
-         } else {
-            console.log('war');
-            if (player1_hand.length < 3){
-               console.log('Player 2 won the game');
-               alert('Player 2 won the game');
-               setGame_on(false);
-               // Increment winner's tally in Database
-               reset();     
+            let card1 = player1_hand.splice(0, 1)[0];
+            let card2 = player2_hand.splice(0, 1)[0];
+             
+            setPlayer1_hand((h1)=> player1_hand)
+            setPlayer2_hand((h2)=> player2_hand)
+
+            setPlayer2_hand((hand2) => [...hand2, card1, card2])
+
+            // setPlayer2_hand([...player2_hand, player2_hand.splice(0, 1)[0], player1_hand.splice(0, 1)[0] ])
+            
+            // while (cards_to_be_won.length > 0){
+            //    setPlayer2_hand([...player2_hand, cards_to_be_won.splice(0, 1)[0] ])
+            // }
+         } //else {
+            // console.log('war');
+            // if (player1_hand.length < 3){
+            //    console.log('Player 2 won the game');
+            //    alert('Player 2 won the game');
+            //    setGame_on(false);
+            //    // Increment winner's tally in Database
+            //    reset();     
                   
-            } else if (player2_hand.length < 3){
-               console.log('Player 2 won the game');
-               alert('Player 1 won the game');
-               setGame_on(false);
-               // Increment winner's tally in Database
-               reset();    
+            // } else if (player2_hand.length < 3){
+            //    console.log('Player 2 won the game');
+            //    alert('Player 1 won the game');
+            //    setGame_on(false);
+            //    // Increment winner's tally in Database
+            //    reset();    
                    
-            } else {
-               cards_to_be_won.push(player1_hand.splice(0, 1)[0])      
-               cards_to_be_won.push(player2_hand.splice(0, 1)[0])       
-               cards_to_be_won.push(player1_hand.splice(0, 1)[0])         
-               cards_to_be_won.push(player2_hand.splice(0, 1)[0])      
+            // } else {
+            //    cards_to_be_won.push(player1_hand.splice(0, 1)[0])      
+            //    cards_to_be_won.push(player2_hand.splice(0, 1)[0])       
+            //    cards_to_be_won.push(player1_hand.splice(0, 1)[0])         
+            //    cards_to_be_won.push(player2_hand.splice(0, 1)[0])      
    
-               playcards()
-            }
-         }
+            //    playcards()
+            // }
+         //}
       }
+      console.log('playCards');
       console.log('player1_hand',player1_hand);
       console.log('player2_hand',player2_hand);
      
@@ -153,6 +192,7 @@ const GameHooks = (props) => {
    return(
       <div>
          <button onClick={reset}>Reset</button>
+         <button onClick={createDeck}>createDeck</button>
          <button onClick={dealCards}>dealCards</button>
          <button onClick={playcards}>playcards</button>
          <button onClick={play_game}>play game!!</button>
